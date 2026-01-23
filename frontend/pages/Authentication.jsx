b8d4716155ac8785933dfc319530a0264ed22665
TheRealTyGuy <101288310+TheRealTyGuy@users.noreply.github.com>
2026-01-22 23:55:33 -0600

    Idek

--- frontend/pages/Authentication.jsx
+++ frontend/pages/Authentication.jsx
@@ -4,13 +4,13 @@ import AuthSubmitButton from "../components/AuthSubmitButton"
 import AuthTypeToggler from "../components/AuthTypeToggler"
 
 function Authentication() {
-    return <>
+    return <div class="flex-column justify-center">
         <h1 class="text-3xl font-bold underline" >Login</h1>
         <EmailInput />
         <PasswordInput />
         <AuthSubmitButton />
         <AuthTypeToggler />
-    </>
+    </div>
 }
 
 export default Authentication;
