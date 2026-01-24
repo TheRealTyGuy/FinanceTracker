a90e3a7498eb978bc1504fb1c8c7077f0cdf89fc
TheRealTyGuy <101288310+TheRealTyGuy@users.noreply.github.com>
2026-01-23 23:51:13 -0600

    Added some more Tailwind to frontend
    The real work today is that I configured
    my desktop and installed dependencies
    and stuff. Using vscodium now for less
    telemetry too

--- frontend/components/EmailInput.jsx
+++ frontend/components/EmailInput.jsx
@@ -1,6 +1,7 @@
 function EmailInput() {
     return <>
-        <h3>EmailInput</h3>
+        <label for="email" className="block text-sm/6 font-medium">Email:</label>
+        <input id="email" type="email" name="email" className="block w-full rounded-md bg-black/10 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
     </>
 }
 
--- frontend/pages/Authentication.jsx
+++ frontend/pages/Authentication.jsx
@@ -4,13 +4,15 @@ import AuthSubmitButton from "../components/AuthSubmitButton"
 import AuthTypeToggler from "../components/AuthTypeToggler"
 
 function Authentication() {
-    return <div class="flex-column justify-center">
-        <h1 class="text-3xl font-bold underline" >Login</h1>
-        <EmailInput />
-        <PasswordInput />
-        <AuthSubmitButton />
-        <AuthTypeToggler />
-    </div>
+    return <div className="flex justify-center">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-3xl font-bold underline" >Login</h1>
+                    <EmailInput />
+                    <PasswordInput />
+                    <AuthSubmitButton />
+                    <AuthTypeToggler />
+            </div>
+        </div>;
 }
 
 export default Authentication;
